b70ae5b5106162e8628e7c33e59166c6711ec843
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-11-28 14:55:11 +0000

    Initial commit of rails app for species+ API

---
+++ config/initializers/apipie.rb
@@ -0,0 +1,9 @@
+Apipie.configure do |config|
+  config.app_name = "CITES Checklist and Species+ API"
+  config.api_base_url = "v1"
+  config.doc_base_url = "/documentation"
+  config.default_version = "v1"
+  config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/v2/*.rb"
+  #config.layout = 'pages'
+  config.copyright = "&copy; #{Time.now.year} UNEP-WCMC"
+end
