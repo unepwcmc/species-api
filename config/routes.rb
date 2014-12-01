8b870de629c05d5fd910108c81ca45539c8ec3ed
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-12-01 14:01:20 +0000

    Added devise registrations controller for custom logic and signing user as API role

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'dashboard/index'
 
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "devise/registrations" }
   apipie
 
   root 'static_pages#index'
