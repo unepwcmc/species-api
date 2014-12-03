840e5e98a1a1ff1acbe209844911cc43e3b1cd0b
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-12-03 12:40:18 +0000

    Changed devise to redirect to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up).push(:name, :terms_and_conditions)
     devise_parameter_sanitizer.for(:account_update) << :name
   end
+
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
     def after_sign_up_path_for(resource)
-      dashboard_path(resource)
+      dashboard_path
     end
 end
