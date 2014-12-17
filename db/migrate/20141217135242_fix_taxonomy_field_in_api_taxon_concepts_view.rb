164236651b5598de0d9193c309bfe15031dc2fc1
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-12-17 13:54:50 +0000

    fixed column definition in taxon concepts view

---
+++ db/migrate/20141217135242_fix_taxonomy_field_in_api_taxon_concepts_view.rb
@@ -0,0 +1,6 @@
+class FixTaxonomyFieldInApiTaxonConceptsView < ActiveRecord::Migration
+  def change
+    drop_view :api_taxon_concepts_view
+    create_view :api_taxon_concepts_view, view_sql('20141217135242', 'api_taxon_concepts_view')
+  end
+end
