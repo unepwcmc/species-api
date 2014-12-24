0892000b9451ecff109a78f74fb92de0fc46ceae
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2014-12-24 09:33:02 +0000

    increased limit of taxon concept list queries

--- app/models/taxon_concept.rb
+++ app/models/taxon_concept.rb
@@ -2,7 +2,7 @@ class TaxonConcept < ActiveRecord::Base
   after_initialize :readonly!
   self.table_name = :api_taxon_concepts_view
   self.primary_key = :id
-  self.per_page = 100
+  self.per_page = 500
 
   has_many :distributions
   has_many :common_names
