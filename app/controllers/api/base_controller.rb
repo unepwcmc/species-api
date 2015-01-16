982ed5c4a6d4a8b864b7a80bf20c207d5f1c1e81
Spencer Dixon <spencerlloyddixon@gmail.com>
2015-01-16 13:31:36 +0000

    Emptied tracking methods to see if fixes issue

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -17,29 +17,29 @@ class Api::BaseController < ApplicationController
 
   # after_action method for recording API Metrics
   def track_this_request
-    ApiRequest.create(
-      user_id: @user.try(:id),
-      controller: params[:controller].split('/').last,
-      action: params[:action],
-      params: params.except(:controller, :action, :format),
-      format: params[:format],
-      ip: request.remote_ip,
-      response_status: response.status
-    )
+    # ApiRequest.create(
+    #   user_id: @user.try(:id),
+    #   controller: params[:controller].split('/').last,
+    #   action: params[:action],
+    #   params: params.except(:controller, :action, :format),
+    #   format: params[:format],
+    #   ip: request.remote_ip,
+    #   response_status: response.status
+    # )
   end
 
   # rescue_from method for recording API Metrics on 500 errors
   def track_this_error(exception)
-    head status: 500 # Manually set this again because we're rescuing from rails magic
-    ApiRequest.create(
-      user_id: @user.try(:id),
-      controller: params[:controller].split('/').last,
-      action: params[:action],
-      params: params.except(:controller, :action, :format),
-      format: params[:format],
-      ip: request.remote_ip,
-      response_status: response.status,
-      error_message: exception.to_s
-    )
+    # head status: 500 # Manually set this again because we're rescuing from rails magic
+    # ApiRequest.create(
+    #   user_id: @user.try(:id),
+    #   controller: params[:controller].split('/').last,
+    #   action: params[:action],
+    #   params: params.except(:controller, :action, :format),
+    #   format: params[:format],
+    #   ip: request.remote_ip,
+    #   response_status: response.status,
+    #   error_message: exception.to_s
+    # )
   end 
 end
