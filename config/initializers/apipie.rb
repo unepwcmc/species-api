4ad7d6bfb1723f2fd5580cf264ed3423d9955387
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2015-04-14 23:48:08 +0100

    added API info to config

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,5 +1,6 @@
 Apipie.configure do |config|
   config.app_name = "CITES Checklist/Species+ API"
+  config.app_info = "Application Programming Interface (API) to support CITES Parties to increase the accuracy and efficiency of curating CITES species data for permitting purposes."
   config.api_base_url = "v1"
   config.doc_base_url = "/documentation"
   config.default_version = "v1"
