dff8c8e227c749914b13c3cb0e15a65138c6dc87
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2016-03-24 08:58:39 +0000

    added rack-cors middleware config to development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,22 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    allow do
+      origins '*'
+
+      resource '/cors',
+        :headers => :any,
+        :methods => [:post],
+        :credentials => true,
+        :max_age => 0
+
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+        :max_age => 0
+    end
+  end
+
 end
