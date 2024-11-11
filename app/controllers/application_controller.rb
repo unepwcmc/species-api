ae27b4e0fb59ceb10da266392069d3cb59090aaf
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-11-11 12:39:37 +0000

    chore: method devise_parameter_sanitizer.for no longer exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,9 @@ class ApplicationController < ActionController::Base
 
     def configure_permitted_parameters
       extra_parameters = [:name, :is_cites_authority, :organisation, :geo_entity_id]
-      devise_parameter_sanitizer.for(:sign_up).push(*extra_parameters, :terms_and_conditions)
-      devise_parameter_sanitizer.for(:account_update).push(*extra_parameters)
+
+      devise_parameter_sanitizer.permit(:sign_up, keys: [ *extra_parameters, :terms_and_conditions ])
+      devise_parameter_sanitizer.permit(:account_update, keys: extra_parameters)
     end
 
     def after_sign_in_path_for(resource)
@@ -22,7 +23,7 @@ class ApplicationController < ActionController::Base
     def authenticate_is_api_or_admin
       unless (params[:controller] == 'registrations') || (params[:controller] == 'devise/sessions')
         if current_user && current_user.is_contributor?
-          sign_out current_user 
+          sign_out current_user
           redirect_to new_user_registration_path, notice: "You do not have permission to access the API, please sign up for an API account"
         end
       end
