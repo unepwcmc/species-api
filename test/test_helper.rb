73ee73625d2a10b114c10a8b4bf2ab6f7304414d
Daniel Perrett <daniel.perrett@unep-wcmc.org>
2024-11-12 12:53:43 +0000

    chore: fix noisy error when testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,20 @@
 require 'simplecov'
+
+ENV['RAILS_ENV'] ||= 'test'
+
 formatters = [SimpleCov::Formatter::HTMLFormatter]
+
 if ENV['CODECLIMATE_REPO_TOKEN']
   require 'codeclimate-test-reporter'
   formatters.push CodeClimate::TestReporter::Formatter
 end
+
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
 SimpleCov.start 'rails'
 SimpleCov.command_name 'test'
 
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'json'
