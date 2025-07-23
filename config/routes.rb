f3e494428e8debdc1b4ae5bc44ebc875dc2a3bd2
Leonardo Wong <leonardo.wong@unep-wcmc.org>
2025-07-23 09:44:28 +0100

    add sidekiq admin page username/password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
+if %w[test development].exclude?(Rails.env)
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    sidekiq_username = Rails.application.credentials.sidekiq.username!
+    sidekiq_password = Rails.application.credentials.sidekiq.password!
+    ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(username),
+      Digest::SHA256.hexdigest(sidekiq_username)) &
+      ActiveSupport::SecurityUtils.secure_compare(Digest::SHA256.hexdigest(password),
+        Digest::SHA256.hexdigest(sidekiq_password))
+  end
+end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
